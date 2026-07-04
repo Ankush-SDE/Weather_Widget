fcea3c1ce397837dda7a2a04b70f51242ec03e96
Ankush Kumar <ankush1524kumar@gmail.com>
2026-07-04 15:35:43 +0530

    infoBox add

---
+++ src/InfoBox.jsx
@@ -0,0 +1,48 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function InfoBox(){
+  const INIT_URL ="https://images.unsplash.com/photo-1722858343990-1604f540c15d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+  let info ={
+    city:"Delhi",
+    feelslike:24.84,
+    temp:25.05,
+    temp:25.05,
+    tempMin:25.05,
+    tempMax:25.05,
+    humidity:47,
+    wearther:"haze",
+  };
+  return(
+    <div className="InfoBox">
+      <h1>WeatherInfo -{info.wearther}</h1>
+      
+       <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={INIT_URL}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {info.city}
+        </Typography>
+        <Typography variant="body2"  color= "text.secondary" component={"span"}>
+          <p>Temperature ={info.temp}&deg;C</p>
+          <p>Humidity ={info.humidity}</p>
+          <p>Min Temp ={info.tempMin}</p>
+          <p>Max Temp={info.tempMax}</p>
+          <p>The weather can be  described as {info.wearther} and feels  like{info.feelslike}&deg;C </p>
+        </Typography>
+      </CardContent>
+      
+    </Card>
+    </div>
+
+  )
+}
