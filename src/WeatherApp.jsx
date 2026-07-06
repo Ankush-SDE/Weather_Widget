899726d65b521ec40bae4d3262406955135e115c
Ankush Kumar <ankush1524kumar@gmail.com>
2026-07-06 17:34:16 +0530

    weatherApp

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,10 +1,47 @@
 
+// import SearchBox from "./SearchBox";
+// import InfoBox from "./InfoBox";
+// import { useState } from "react";
+// export default function WeatherApp(){
+//   const[weatherInfo,setWeatherInfo] = useState({
+//     city:"Ankush",
+//     feelslike:24.84,
+//     temp:25.05,
+//     temp:25.05,
+//     tempMin:25.05,
+//     tempMax:25.05,
+//     humidity:47,
+//     wearther:"haze",
+//   })
+//   return (
+//     <div style={{textAlign:"center"}}>
+      
+//       <h2>WeatherApp</h2>
+//       <SearchBox/>
+//       <InfoBox info={weatherInfo}/>
+//     </div>
+//   );
+// }
 import SearchBox from "./SearchBox";
 import InfoBox from "./InfoBox";
-export default function Weather(){
+import { useState } from "react";
+
+export default function WeatherApp() {
+  const [weatherInfo, setWeatherInfo] = useState({
+    city: "Ankush",
+    feelslike: 24.84,
+    temp: 25.05,
+    tempMin: 25.05,
+    tempMax: 25.05,
+    humidity: 47,
+    weather: "Haze",
+  });
+
   return (
-    <div>
-      <h2>WeatherApp</h2>
+    <div style={{ textAlign: "center" }}>
+      <h2>Weather App</h2>
+      <SearchBox />
+      <InfoBox info={weatherInfo} />
     </div>
-  )
+  );
 }
