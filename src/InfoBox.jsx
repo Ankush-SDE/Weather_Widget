57e0d49e6bd24e8851b4ae46319840ddd7b7965a
Ankush Kumar <ankush1524kumar@gmail.com>
2026-07-08 17:22:10 +0530

    add image

--- src/InfoBox.jsx
+++ src/InfoBox.jsx
@@ -4,10 +4,16 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import "./InfoBox.css"
+import "./InfoBox.css";
+
+// import AcUnitIcon from '@mui/icons-material/AcUnit';
+// import SunnyIcon from '@mui/icons-material/Sunny';
+// import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
 export default function InfoBox({info}){
   const INIT_URL ="https://images.unsplash.com/photo-1722858343990-1604f540c15d?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZHVzdHklMjB3ZWF0aGVyfGVufDB8fDB8fHww";
-  
+  const HOT_URL ="https://images.unsplash.com/photo-1700756102943-fa388b2ed119?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3VtbWVyJTIwaW1hZ2UlMjByYWphc3RoYW4lMjBzdW58ZW58MHx8MHx8fDA%3D";
+  const COLD_URL ="https://images.unsplash.com/photo-1640954941708-58bf3fd48a3e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGNvbGQlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D";
+  const RAIN_URL ="https://plus.unsplash.com/premium_photo-1670002347718-de1091111728?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHJhaW4lMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D";
   return(
     <div className="InfoBox">
       {/* <h1>WeatherInfo -{info.wearther}</h1> */}
@@ -17,11 +23,20 @@ export default function InfoBox({info}){
         component="img"
         alt="green iguana"
         height="140"
-        image={INIT_URL}
+        image={info.humidity>80?RAIN_URL:info.temp>15?HOT_URL:COLD_URL}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {info.city}
+          {/* {info.humidity>80 ?(
+            <ThunderstormIcon/>
+          ):info.temp>15?(<SunnyIcon/>
+
+          ):(
+            <AcUnitIcon/>
+          )
+        } */}
+
         </Typography>
         <Typography variant="body2"  color= "text.secondary" component={"span"}>
           <p>Temperature ={info.temp}&deg;C</p>
